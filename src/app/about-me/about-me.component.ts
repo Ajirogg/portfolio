d84a58bfd5afeb6d4d59f7f4f9d305035bebb7cd
Ajirogg <raimundo.guillaume@gmail.com>
2020-09-14 19:07:55 +0200

    Mise en place A propos + img logo

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { GalleriaThumbnails } from 'primeng/galleria';
+import creationsData from "src/data/game.json";
+
 
 @Component({
   selector: 'app-about-me',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutMeComponent implements OnInit {
 
+  text: string[];
+  email: string;
+  linkedin: string;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.findAboutInfo();
+    console.log(this.text);
   }
 
+  findAboutInfo(): void {
+    this.email = creationsData.about.email;
+    this.linkedin = creationsData.about.linkedin;
+    this.text = creationsData.about.text;
+  }
 }
